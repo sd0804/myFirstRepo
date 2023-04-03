efae468631c7a2807541db89926263570af88884
karuna <sharmakaruna1610@gmail.com>
2023-04-03 08:48:59 +0530

    commit1

---
+++ src/main/java/com/example/responseentity/ResponseentityApplication.java
@@ -0,0 +1,28 @@
+package com.example.responseentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ResponseentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseentityApplication.class, args);
+	}
+
+	@GetMapping("/myfisrtapi")
+	public ResponseEntity<String> getEmployee()
+	{
+		String body="Hello World";
+		HttpHeaders h=new HttpHeaders();
+		h.add("myfirsTHEADER","i LOVE");
+		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(body);
+	}
+
+}
