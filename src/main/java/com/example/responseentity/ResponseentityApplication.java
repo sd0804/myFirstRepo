97884946cbc51041d3857b7732a7ee41d3da1d37
karuna <sharmakaruna1610@gmail.com>
2023-07-23 19:12:13 +0530

    change2

--- src/main/java/com/example/responseentity/ResponseentityApplication.java
+++ src/main/java/com/example/responseentity/ResponseentityApplication.java
@@ -24,6 +24,7 @@ public class ResponseentityApplication {
 		h.add("myfirsTHEADER","i LOVE");
 		return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(body);
 		//my chane1
+		//my change2
 
 	}
 
